f0b5166e181683f9d7fcf60c1d08e71494c0b943
Elis <Byhovtsov@gmail.com>
2021-10-05 09:45:20 +0300

    Start

---
+++ src/main/java/com/privateadvertisements/PrivateAdvertisementsApplication.java
@@ -0,0 +1,13 @@
+package com.privateadvertisements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateAdvertisementsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrivateAdvertisementsApplication.class, args);
+    }
+
+}
